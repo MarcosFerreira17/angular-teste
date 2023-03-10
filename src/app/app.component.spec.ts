caa2e5290b2a4472439ca9852206b9a487fde6c0
mnia <marcos.ferreira@gft.com>
2023-03-10 15:54:21 -0300

    a

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'teste'`, () => {
+  it(`should have as title 'angular-teste'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('teste');
+    expect(app.title).toEqual('angular-teste');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('teste app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-teste app is running!');
   });
 });
